70b14f734a196fd5bdbb80908a173b5ddc56914a
Anna Angelina <brito.angelina78@gmail.com>
2026-06-25 09:34:05 -0300

    ajustando views

--- escolaIFRN/biblioteca/views.py
+++ escolaIFRN/biblioteca/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
-def listLivros(request):
+def livrosreturn():
     livros = [{'id': 1, 'titulo': 'A cabeça do santo', 'autor': 'Socorro Acioli', 'ano': 2014, 'prioridade_leitura': 2, 'sinopse': 'Samuel encontra abrigo dentro da cabeça de uma enorme estátua de santo e sua vida passa por transformações inesperadas.'},
               {'id': 2, 'titulo': 'A hora da estrela', 'autor': 'Clarice Lispector', 'ano': 1977, 'prioridade_leitura': 1, 'sinopse': 'A história de Macabéa, uma jovem nordestina que vive à margem da sociedade no Rio de Janeiro.'},
               {'id': 3, 'titulo': 'Vidas Secas', 'autor': 'Graciliano Ramos', 'ano': 1938, 'prioridade_leitura': 1, 'sinopse': 'Uma família sertaneja enfrenta a seca e a miséria no interior do Nordeste brasileiro.'},
               {'id': 4, 'titulo': 'Uma pequena coreografia do Adeus', 'autor': 'Aline Bei', 'ano': 2021, 'prioridade_leitura': 3, 'sinopse': 'Um romance poético sobre afetos, perdas e reconstrução emocional de uma garota chamada Júlia'}]
-    context = {'livros': livros}
+    return livros
+
+# Create your views here.
+def listLivros(request):
+    context = {'livros': livrosreturn()}
     return render(request, 'biblioteca/list-livros.html', context)
 
 def livroDetalhe(request, id):
-    livros = [{'id': 1, 'titulo': 'A cabeça do santo', 'autor': 'Socorro Acioli', 'ano': 2014, 'prioridade_leitura': 2, 'sinopse': 'Samuel encontra abrigo dentro da cabeça de uma enorme estátua de santo e sua vida passa por transformações inesperadas.'},
-            {'id': 2, 'titulo': 'A hora da estrela', 'autor': 'Clarice Lispector', 'ano': 1977, 'prioridade_leitura': 1, 'sinopse': 'A história de Macabéa, uma jovem nordestina que vive à margem da sociedade no Rio de Janeiro.'},
-            {'id': 3, 'titulo': 'Vidas Secas', 'autor': 'Graciliano Ramos', 'ano': 1938, 'prioridade_leitura': 1, 'sinopse': 'Uma família sertaneja enfrenta a seca e a miséria no interior do Nordeste brasileiro.'},
-            {'id': 4, 'titulo': 'Uma pequena coreografia do Adeus', 'autor': 'Aline Bei', 'ano': 2021, 'prioridade_leitura': 3, 'sinopse': 'Um romance poético sobre afetos, perdas e reconstrução emocional de uma garota chamada Júlia'}]
-    for livro in livros:
+    for livro in livrosreturn():
         if livro['id'] == id:
             livroselecionado = livro
     context = {'livro_selecionado': livroselecionado}
